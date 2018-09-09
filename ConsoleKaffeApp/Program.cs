614d7b775b925b46a00094347aafe98ad76f3f96
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 22:01:30 +0200

    FaltWhite. things i didt add. now making testclass

--- ConsoleKaffeApp/Program.cs
+++ ConsoleKaffeApp/Program.cs
@@ -9,9 +9,11 @@ namespace ConsoleKaffeApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
+            FaltWhite FW1 = new FaltWhite();
+            Console.WriteLine($"add {FW1.MlMælk()} Ml. Mælk. prisen er {FW1.Pris()}kr. Stykken er {FW1.Styrke()}");
+            Console.ReadLine();
         }
     }
 }
