7b15b5cf5a0272f4dfc1dfe620d4f2178fc0b527
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 21:11:09 +0200

    FaltWhite. Pris() & Stykke() add

--- kaffe/FaltWhite.cs
+++ kaffe/FaltWhite.cs
@@ -9,10 +9,21 @@ namespace kaffe
     public class FaltWhite : Kaffe , Imælk
     {
         // Return mælk in a faltwhite in Milliters 
-        public int mlmælk()
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
         {
             return 45;
         }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
 
     }
 }
