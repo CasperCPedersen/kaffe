0e132cf2349fc944e4617384eb587c2d43c383b2
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 21:01:43 +0200

    FaltWhite class and mlmælk metod

---
+++ kaffe/FaltWhite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FaltWhite : Kaffe , Imælk
+    {
+        // Return mælk in a faltwhite in Milliters 
+        public int mlmælk()
+        {
+            return 45;
+        }
+
+    }
+}
